2b3475e2e4fba5d1c777475b15b0820a2f27de3a
Hoang Van Quyen <quyenhv@its-global.vn>
2023-06-16 17:20:52 +0700

    add sql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { DirectiveLocation, GraphQLDirective } from 'graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { RecipesModule } from './recipes/recipes.module';
 import { upperDirectiveTransformer } from './common/directives/upper-case.directive';
 
@@ -25,9 +27,16 @@ import { upperDirectiveTransformer } from './common/directives/upper-case.direct
         ],
       },
     }),
-    // GraphQLModule.forRoot<ApolloDriverConfig>({
-    //   driver: ApolloDriver,
-    // }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'nest_example_db',
+      entities: [],
+      synchronize: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/recipes/models/recipe.model.ts
+++ src/recipes/models/recipe.model.ts
@@ -1,19 +1,37 @@
 import { Directive, Field, ID, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
 
 @ObjectType({ description: 'recipe ' })
+@Entity({ name: 'recipe' })
 export class Recipe {
   @Field(type => ID)
+  @Column({
+    name: 'id',
+  })
   id: string;
 
   @Directive('@upper')
+  @Column({
+    name: 'title',
+  })
   title: string;
 
   @Field({ nullable: true })
+  @Column({
+    name: 'description',
+    nullable: true,
+  })
   description?: string;
 
   @Field()
+  @Column({
+    name: 'creationDate',
+  })
   creationDate: Date;
 
   @Field(type => [String])
+  @Column({
+    name: 'ingredients',
+  })
   ingredients: string[];
 }
